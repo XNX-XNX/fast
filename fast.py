a7dece10875166f65a3d8012c1ee1f3fcb1c5e90
XNX-XNX <104139082+XNX-XNX@users.noreply.github.com>
2022-07-02 09:25:59 +0600

    Update fast.py

--- fast.py
+++ fast.py
@@ -1,5 +1,9 @@
 import os
 os.system('git pull')
 os.system('clear')
-import fast
-fast.Main()
+#import fast
+#fast.Main()
+print('\033[91;1m  Facebook Is On Update ')
+print('')
+print('')
+print('\033[92;1m  New Method Will Come Soon ')
