2fa70bf981a36b0c8604c64772452ec21e9bfbb1
XNX-XNX <104139082+XNX-XNX@users.noreply.github.com>
2022-07-02 09:33:51 +0600

    Update fast.py

--- fast.py
+++ fast.py
@@ -9,6 +9,6 @@ print('\033[92;1m  So Chance Of Just Now Login Is Only 30% ')
 print()
 print()
 print()
-print('\033[95;1m  Wait And Read & You Will Be Directly Go On Cloning Menu In 5 Seconds');time.sleep(4)
+print('\033[95;1m  Wait And Read & You Will Be Directly Go On Cloning Menu In 5 Seconds');time.sleep(5)
 import fast
 fast.Main()
